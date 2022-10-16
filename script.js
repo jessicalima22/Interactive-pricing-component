e5a8fc7918a4b32b6588e6825755441370fcabab
jessicalima22 <jessica.lima22@hotmail.com>
2022-10-16 17:07:26 -0300

    Toggle js backup

--- script.js
+++ script.js
@@ -1,46 +1,48 @@
 const input = document.getElementById('range');
 const output = document.getElementById('price');
 const pageviews = document.getElementById('pageviews');
-const arrayInitialPrices = ['8', '12', '16', '24', '36'];
+const arrayInitialPrices = [8, 12, 16, 24, 36];
 const arrayPageviews = ['10K','50K','100K','500K','1M',];
+inputValue = input.value;
 
 
 
-//input range
 
-input.addEventListener('input', function (){
+ //input range
+
+ input.addEventListener('input', function (){
     inputValue = input.value;
         output.innerText = arrayInitialPrices[inputValue];
         pageviews.innerText = arrayPageviews[inputValue];
     });
 
-
+//verificar como colocar nome na função change e chamar ela dentro do listener do input
 
 
 //toggle checked
 
-const toggle = document.getElementById('toggle');
-toggle.addEventListener('click', function (){
+const toggle = document.getElementById('toggle')
+toggle.addEventListener('change', function (){
+
+    const discount = arrayInitialPrices.map(item => item * 0.25);
+    const arrayLowerPrices = arrayInitialPrices.map(item => item - discount)
+
+    console.log(arrayInitialPrices);
+    console.log(discount);
+    console.log(arrayLowerPrices);
+
     
-    const outputValue = output.innerText;
-    const price = Number(outputValue);
-    const discount = price*0.25;
-    const lowerPrice = price - discount;
-    inputValue = input.value;
     console.log(toggle.checked);
 
-    if (toggle.checked === true){
-    output.innerText = lowerPrice;
-    } else { 
-    output.innerText = arrayInitialPrices[inputValue];
-    }
-});
+    
 
-
-
-
-
-
+        if (toggle.checked === false){
+        output.innerText = arrayInitialPrices[inputValue];
+        } else {
+        output.innerText = arrayLowerPrices[inputValue];
+        }
+        
+    });
 
 
 
